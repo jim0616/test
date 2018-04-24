bcaf485380a480cf691f1c329690de85b4345ace
jim.hsu <jim.hsu@tungthih.com.tw>
2018-04-24 08:19:27 +0800

    改變位置

--- ConnectionTest/ConnectionTest/Form1.Designer.cs
+++ ConnectionTest/ConnectionTest/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 89);
+            this.button1.Location = new System.Drawing.Point(89, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
